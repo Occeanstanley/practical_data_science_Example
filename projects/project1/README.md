163d964cbecabfbc0bee70383416a3b307cc9777
Occeanstanley <105721778+Occeanstanley@users.noreply.github.com>
2025-02-15 05:38:12 +0000

    readme file added for project 1

---
+++ projects/project1/README.md
@@ -0,0 +1,3 @@
+## README
+
+this is a folder for project1. please see *deliverable1.py* as the first deliverble. please see *deliverable2.py* as the second deliverable the *test_run_v2.py* is for the second deliverable.
--- projects/project1/test_run_v2.py
+++ projects/project1/test_run_v2.py
@@ -1,4 +1,17 @@
 from deliverable2 import *
 
+class URLEvaluator:
+    def __init__(self, user_prompt, url_to_check):
+        self.user_prompt = user_prompt
+        self.url_to_check = url_to_check
+
+    def display_info(self):
+        print(f"User Query: {self.user_prompt}")
+        print(f"URL to Evaluate: {self.url_to_check}")
+
+# Example usage
 user_prompt = "Are there any health risks for my newborn if I return home after an international flight?"
 url_to_check = "https://www.mayoclinic.org/healthy-lifestyle/infant-and-toddler-health/expert-answers/air-travel-with-infant/faq-20058539"
+
+url_eval = URLEvaluator(user_prompt, url_to_check)
+url_eval.display_info()
